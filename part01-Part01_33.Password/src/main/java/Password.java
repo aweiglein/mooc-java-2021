2405660ac59df55113a017f3a7e747bf4ca87f8a
Alyssa Weiglein <weigleina@gmail.com>
2021-04-29 20:50:17 -0700

    solved

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -6,6 +6,15 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        System.out.println("Password?");
+        String text = scan.nextLine();
+        
+        String pw = "Caput Draconis";
+                
+        if (text.equals(pw)) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
