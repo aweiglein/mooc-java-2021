bf1efedeb867e5d2023d4ca90c8f312dc0208cc5
Alyssa Weiglein <weigleina@gmail.com>
2021-04-30 15:20:08 -0700

    solved

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,20 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num < 0) {
+                System.out.println("Unsuitable number");
+            }
+            if (num > 0) {
+                int pos = num * num;
+                System.out.println(pos);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
     }
 }
