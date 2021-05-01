98396581da5b53bd44b7cc64b334e108363ca285
Alyssa Weiglein <weigleina@gmail.com>
2021-04-30 22:57:13 -0700

    solved

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -3,8 +3,12 @@
 public class FromParameterToOne {
 
     public static void main(String[] args) {
-
-
+        printFromNumberToOne(2);
     }
 
+    public static void printFromNumberToOne(int num) {
+        for (int i = num; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
 }
--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,14 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        divisibleByThreeInRange(2, 20);
     }
-
+    
+    public static void divisibleByThreeInRange (int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 3 ==0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
